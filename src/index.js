eec8035ce2e00b73380e95f7932aac070937093e
liuderchi <liuderchi@gmail.com>
2018-04-26 18:20:04 +0800

    introduce dotenv-safe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
+=======
+require('dotenv-safe').config();
+
+console.log(`How are you, ${process.env.OWNER}?`);
+>>>>>>> introduce dotenv-safe
