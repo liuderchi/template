83246a4245bd8a4f506dbfab5c9a3bf755035b6b
Te-Chi Liu <liuderchi@gmail.com>
2018-04-26 15:01:27 +0800

    chore(root): remove redundant docs, dependencies; add editorconfig, update travis

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
